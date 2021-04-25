abfb587765a7f21622b83a78fe1722e33c22773f
Dinesh Tummlapalli <dinesh.tummlapalli@mrcooper.com>
2021-04-25 09:45:09 -0500

    Heap Implemented

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/dineshtbits/data-structures-in-go
 
 go 1.14
 
-require golang.org/x/tools v0.1.0
+require (
+	golang.org/x/text v0.3.3
+	golang.org/x/tools v0.1.0
+)
---
+++ heap/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Heap struct {
+	items []int
+}
+
+func (h *Heap) Insert(element int) {
+	h.items = append(h.items, element)
+	h.heapifyUp()
+}
+
+func (h *Heap) Extract() {
+	h.items[0] = h.items[len(h.items)-1]
+	h.items = h.items[:len(h.items)-1]
+	h.heapifyDown()
+}
+
+func (h *Heap) heapifyUp() {
+	for start := len(h.items) - 1; h.items[parent(start)] < h.items[start]; start = parent(start) {
+		h.swap(parent(start), start)
+	}
+}
+
+func (h *Heap) heapifyDown() {
+	for start := 0; true; {
+		largerChild := h.largerChildIndex(start)
+		if largerChild <= -1 {
+			return
+		}
+		if h.items[largerChild] > h.items[start] {
+			h.swap(largerChild, start)
+		}
+		start = largerChild
+	}
+}
+
+func (h *Heap) largerChildIndex(start int) int {
+	left := 2*start + 1
+	right := 2*start + 2
+
+	if left >= len(h.items) && right >= len(h.items) {
+		return -1
+	} else if left < len(h.items) && right < len(h.items) {
+		if h.items[left] > h.items[right] {
+			return left
+		} else {
+			return right
+		}
+	} else if right >= len(h.items) {
+		return left
+	} else {
+		return right
+	}
+}
+
+func (h *Heap) swap(left, right int) {
+	h.items[left], h.items[right] = h.items[right], h.items[left]
+}
+
+func parent(index int) int {
+	return (index - 1) / 2
+}
+
+func main() {
+	h := &Heap{}
+	fmt.Printf("initial value: %v \n", h)
+	arr := []int{5, 7, 2, 9, 3, 6, 1, 4, 10}
+	for _, v := range arr {
+		h.Insert(v)
+	}
+	fmt.Printf("heapified value: %v \n", h)
+	h.Extract()
+	fmt.Printf("Extracted value: %v \n", h)
+	h.Extract()
+	fmt.Printf("Extracted value: %v \n", h)
+	h.Extract()
+	fmt.Printf("Extracted value: %v \n", h)
+}
