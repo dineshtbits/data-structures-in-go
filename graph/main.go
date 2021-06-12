2939a3240523a32d0ffc2d8d8088b390ee5a884c
Dinesh Tummlapalli <dinesh.tummlapalli@mrcooper.com>
2021-06-11 22:32:56 -0500

    BFS & DFS

--- graph/main.go
+++ graph/main.go
@@ -1,6 +1,14 @@
 package graph
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/dineshtbits/data-structures-in-go/queue"
+)
+
+type GraphMatrix struct {
+	edges [][]int
+}
 
 type Graph struct {
 	nodes []*Node
@@ -15,8 +23,81 @@ func New() *Graph {
 	return &Graph{nodes: []*Node{}}
 }
 
+func (g *Graph) visitDfs(id int, visited map[int]bool) {
+	visited[id] = true
+	fmt.Printf("%v ", id)
+	for k := range g.nodes[id].edges {
+		if !visited[k] {
+			g.visitDfs(k, visited)
+		}
+	}
+}
+
+func (g *Graph) DFS() {
+	visited := make(map[int]bool)
+	for i := 0; i < len(g.nodes); i++ {
+		if !visited[g.nodes[i].id] {
+			g.visitDfs(g.nodes[i].id, visited)
+		}
+	}
+}
+
+func (g *Graph) visitBFS(node int, visited map[int]bool) {
+	q := queue.Queue{}
+
+	visited[node] = true
+	q.Enqueue(node)
+
+	for q.Size() > 0 {
+		n := q.Dequeue()
+		fmt.Printf("%v", n)
+
+		for k := range g.nodes[n.(int)].edges {
+			if !visited[k] {
+				visited[k] = true
+				q.Enqueue(k)
+			}
+		}
+	}
+}
+
+func (g *Graph) BFS() {
+	visited := make(map[int]bool)
+	for i := 0; i < len(g.nodes); i++ {
+		if !visited[g.nodes[i].id] {
+			g.visitBFS(g.nodes[i].id, visited)
+		}
+	}
+}
+
+func NewMatrix(nodes int) *GraphMatrix {
+	m := &GraphMatrix{edges: [][]int{}}
+	for i := 0; i < nodes; i++ {
+		row := []int{}
+		for j := 0; j < nodes; j++ {
+			row = append(row, 0)
+		}
+		m.edges = append(m.edges, row)
+	}
+	return m
+}
+
 func (g *Graph) AddNode() {
 	g.nodes = append(g.nodes, &Node{id: len(g.nodes), edges: make(map[int]int)})
+}
+
+func (m *GraphMatrix) AddMatrixNode() {
+	row := []int{}
+	for i := 0; i < len(m.edges); i++ {
+		m.edges[i] = append(m.edges[i], 0)
+		row = append(row, 0)
+	}
+	row = append(row, 0)
+	m.edges = append(m.edges, row)
+}
+
+func (m *GraphMatrix) AddMatrixEdge(from, to, weight int) {
+	m.edges[from][to] = weight
 }
 
 func (g *Graph) ShowNodes() {
